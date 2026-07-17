4bc54ded4bb7e48bf094a5c2369dc1eebf61ecb6
balakoyyamani <balakoyyamani@gmail.com>
2026-07-17 18:25:43 +0530

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from google import genai
+import os
+
+api=os.getenv("GOOGLE_API_KEY_ASSIST_BOT")
+client=genai.Client(api_key=api)
+
+print(api)
+
+history=[]
+
+print("AI ASSIST BOT")
+print("type 'exit' to quit \n")
+
+while True:
+    user=input("user : ")
+    if user.lower()=="exit":
+        break
+    history.append(f"User: {user}")
+    prompt="".join(history)
+
+    response=client.models.generate_content(
+        model="gemini-3.5-flash",
+        contents=prompt
+    )
+
+    answer=response.text
+
+    print(f"AI Bot: {answer}")
+
+    history.append(f"Assistant: {answer}")
