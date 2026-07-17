e762294552ddf11404235eb99448301b38bf334a
balakoyyamani <balakoyyamani@gmail.com>
2026-07-17 19:30:32 +0530

    feat: detect the working model

--- app.py
+++ app.py
@@ -4,22 +4,41 @@ import os
 api=os.getenv("GOOGLE_API_KEY_ASSIST_BOT_02")
 client=genai.Client(api_key=api)
 
-print(api)
+print(f"API KEY : {api}")
+
+working_model=None
+
+for model in client.models.list():
+    model_name=model.name.replace("models/","")
+    try:
+        client.models.generate_content(
+        model=model_name,
+        contents="Hello")
+        working_model=model_name
+        print(f"Working Modl : {model_name}")
+        break
+    except Exception:
+            continue
+    
+if working_model is None:
+     print("There is no Working model available now")
+     exit()
+
 
 history=[]
 
 print("AI ASSIST BOT")
-print("type 'exit' to quit \n")
+print("(type 'exit' to quit) \n")
 
 while True:
     user=input("user : ")
     if user.lower()=="exit":
         break
     history.append(f"User: {user}")
-    prompt="".join(history)
+    prompt="\n".join(history)
 
     response=client.models.generate_content(
-        model="gemini-3.5-flash",
+        model=working_model,
         contents=prompt
     )
 
