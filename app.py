b3e3f4fad7c577205e4d404c114e68c9f667d898
balakoyyamani <balakoyyamani@gmail.com>
2026-07-20 15:49:09 +0530

    feat: flask intiated

--- app.py
+++ app.py
@@ -1,40 +1,9 @@
-from config import get_working_model
-from chat import chat
-from history import load_history,save_history,clear_history
+from flask import Flask
+app=Flask(__name__)
 
-model=get_working_model()
-history=load_history()
+@app.route("/")
+def home():
+    return "Welcome to AI ASSIST BOT 🤖"
 
-print("=" * 45)
-print("AI ASSIST TUTOR")
-print("=" * 45)
-
-while True:
-    user=input("User : ").strip()
-    if not user:
-        print("Enter the Message")
-        continue
-    if user=="exit":
-        break
-    if user=="/clear":
-        clear_history()
-        print("Memory are deleted 😎")
-        continue
-    if user=="/history":
-          print("HISTORY")
-          print("=" *45+"\n")
-          for data in load_history():
-               print(data)
-               print("\n"+"_"*100+"\n")
-          continue
-    if user=="/help":
-        print(""" 
-              'exit' : to quit the chat. 
-              '/clear' : to delete all history. 
-              '/history' : to display the history. 
-              '/help' : to see available commands""")
-        continue
-    history=chat(model,history,user)
-    save_history(history)
-    
-print("Thanks You 😊")
+if __name__=="__main__":
+    app.run(debug=True)
